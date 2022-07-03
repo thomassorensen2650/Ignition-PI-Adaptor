3a61111a3169348df6b48771b35d47b5780375b9
Thomas <thomassorensen2650@gmail.com>
2022-07-03 06:39:59 +0200

    Started refactoring the Path code

---
+++ Integration-PI-gateway/src/main/java/com/unsautomation/ignition/piintegration/PathUtilities.java
@@ -0,0 +1,20 @@
+package com.unsautomation.ignition.piintegration;
+
+import com.inductiveautomation.ignition.common.QualifiedPath;
+import com.unsautomation.ignition.piintegration.piwebapi.PIObjectType;
+
+public class PathUtilities {
+
+    public static PIObjectType findPathType(QualifiedPath path) {
+        return PIObjectType.Unknown;
+    }
+
+    public static QualifiedPath AppendToPath(String pathItem) {
+        return null;
+    }
+
+    public QualifiedPath createPath(String path, Boolean hasChildren) {
+        return null;
+    }
+
+}
